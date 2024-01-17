88e5990a5a3ae261292bb5ea7be1c6372a840c14
Fraulo23 <fraulinifederico@gmail.com>
2024-01-17 16:30:39 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio=new Radio(70);
+            Console.WriteLine(radio.Volume);
+            Console.WriteLine(radio.AlzaVolume());
+            Console.WriteLine(radio.DimunuisciVolume());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Radio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    internal class Radio
+    {
+        public float Volume { get; set; }
+        public Radio(float volume)
+        {
+            Volume = volume;
+        }
+        public float AlzaVolume()
+        {
+            Volume+=(float)(Volume*0.1);
+            return Volume;
+        }
+        public float DimunuisciVolume()
+        {
+            Volume -= (float)(Volume * 0.1);
+            return Volume;
+        }
+    }
+}
